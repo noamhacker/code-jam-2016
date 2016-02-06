6ccf88ceaf288037d9464a01451d5fda45facf1d
amerisrudland <13akar@queensu.ca>
2016-02-06 19:21:40 +0000

    forms: resources. In-progress

--- battle_of_bands/app/controllers/bands_controller.rb
+++ battle_of_bands/app/controllers/bands_controller.rb
@@ -1,5 +1,39 @@
 class BandsController < ApplicationController
+    def index
+    #  if params[:status] == "activated"
+    #   @band = Band.activated
+    #  else
+    #   @band = Band.inactivated
+    #  end
+        @band = Band.all
+    end
+    
     def new
         @band = Band.new
     end
+    
+    def create
+      @band = Band.new(band_params[:band])
+    if @band.save
+      redirect_to @band
+    else
+      # This line overrides the default rendering behavior, which
+      # would have been to render the "create" view.
+      render "new"
+     end
+    end
+    
+    def edit
+    
+    end
+    
+    def show
+        
+    end
+    
+    private
+    def band_params
+      params.require(:band).permit(:name, :website)
+    end
+
 end
