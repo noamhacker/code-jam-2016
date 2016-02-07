ddcc323d48d1ffae56a1df97afd1ed5e3617785a
Noam Hacker <nhacker95@gmail.com>
2016-02-07 00:05:49 +0000

    add admin route and other fixes

--- battle_of_bands/config/routes.rb
+++ battle_of_bands/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/about' => 'static_pages#about'
 
   get '/contact' => 'static_pages#contact'
+  
+  get '/admin' => 'bands#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
