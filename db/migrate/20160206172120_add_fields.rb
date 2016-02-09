34f731c7c71882a61a4d28c24cd8062a4c78aac0
Noam Hacker <nhacker95@gmail.com>
2016-02-09 04:17:32 +0000

    fix migration to be deployable on heroku

--- db/migrate/20160206172120_add_fields.rb
+++ db/migrate/20160206172120_add_fields.rb
@@ -1,8 +1,8 @@
 class AddFields < ActiveRecord::Migration
   def change
     
-    add_column :bands, :name, :string
-    add_column :bands, :website, :string
+    #add_column :bands, :name, :string
+    #add_column :bands, :website, :string
     add_column :bands, :bio, :text
     add_column :bands, :location, :string
     add_column :bands, :score, :integer
