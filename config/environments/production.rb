9674a49d1ea69ed628c672c32e846d0aeaa45142
Noam Hacker <nhacker95@gmail.com>
2016-02-16 21:04:27 +0000

    fix precompile issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,6 @@ Rails.application.configure do
   # these three will make the images load
   config.serve_static_assets = true
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-  config.assets.compile = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -34,7 +33,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
